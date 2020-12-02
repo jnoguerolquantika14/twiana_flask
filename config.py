938347f2a109985a9c6151095de70a17d4dfd78d
juanogtir <juanogtir@alum.us.es>
2020-12-02 12:45:49 +0100

    Initial commit
    Se han unificado los dos repositorios anteriores

---
+++ config.py
@@ -0,0 +1,32 @@
+# CONF TWIANA
+mongo = True
+log = True
+friends = False
+verbose = False
+path_log = ""
+
+##-----------------VARIABLES TWIANA---------------------##
+
+# DIFERENCIAS
+dif_nor = 2  # Diferencia de puntos para adjudicar resultado al usuario
+
+# API
+# Limite de tweets de la API cuando el usuario no se encuentra en la db
+limite_tweets_apionly = 100
+# Limite de tweets de la API cuando el usuario se encuentra en la db
+limite_tweets_apiupdate = 100
+limite_friends = 20  # Limite de friends de la API
+
+# BD
+# Limite de tweets para analizar de la DB (sin limite, modules/analiticsOperations.py para activarlo)
+limite_tweets_db = 100
+
+# PUNTUACIONES
+puntuacion_tweet_positivo = 5
+puntuacion_tweet_negativo = 5
+puntuacion_tweet_neutro = 1
+puntuacion_description = 26
+puntuacion_nombre = 100
+puntuacion_account = 100
+puntuacion_friends_description = 10
+puntuacion_friends_name = 5
---
+++ keys.py
@@ -0,0 +1,209 @@
+def twiana_key(option):
+    if option == "1":
+        # testinng
+        return {'consumer_key': 'FY4O5eQY1FngFz9IA2rHeXu5l',
+                'consumer_secret': 'ep4dxJYzjN3iVfzbaHYWofCr3R9X4h4Q8Qq4YMmpPIsFL9a7ZG',
+                'access_token_key': '272079833-keAYUAXLi2EMCP73YKITkMoVnmts1iZQrI33ttSm',
+                'access_token_secret': 'j6vnfOuiYVVHto9YwbBH9fSWPcUqxBwf4OFKIv7WUSr8W'}
+    # elif option == "1":
+    #     # Tuit Elecciones Token
+    #     return {'consumer_key': 'UVlWJxXPhjCKb1OuZ4FzJvFaZ',
+    #             'consumer_secret': 'GgVPm9R944VFUNelZTNO26z4u1r95KayDJFBu0NPt9uYuJHYLf',
+    #             'access_token_key': '1275018582-ncdpz54VfeM9vpHym6OdFtwYwrnHYm62a2le1cY',
+    #             'access_token_secret': 'hLKJD6wOLv2qSQvmFZPJX5N4LPmXNV7cvtT1YU7rvxsoy'}
+    elif option == "2":
+        # QuantiKa14 Token
+        return {'consumer_key': 'UVlWJxXPhjCKb1OuZ4FzJvFaZ',
+                'consumer_secret': 'Oc5HDs2orqP9sleOmfJGwwfAvWwU7OiIYCxLbcMEw082nrjH1N',
+                'access_token_key': '1598317591-L5F391wFAtt95B7jJ0yAoNXDd1sChgfwb2zYC2C',
+                'access_token_secret': 'F8WQcRr0u65azkETTGOUZfeIvn8XTZg8kJw1uI7iUZ8Gw'}
+    elif option == "3":
+        # QuantiKa14 Token 2
+        return {'consumer_key': 'ghZuLXPIQUwAezavhQUd52KaR',
+                'consumer_secret': 'DW8bUX69rKW17XI8TfYbkimGSHGGvRcMC6u8LFF89VUkhHedhX',
+                'access_token_key': '1598317591-b8jyktseuE8Yiv30SBOM5dEGwfiM9X7rjW2Ltyi',
+                'access_token_secret': 'dgnY7Kw8toMxPctjlVspLvxQDDXybDpewWx5PDFRnzdQg'}
+    elif option == "4":
+        # JorgeWebsec Token
+        return {'consumer_key': 'yKnhwGcg7HTNHKykQOrIJKIeY',
+                'consumer_secret': 'nN7V6fVHxZMACkKX8l1xfBEDudTR9YtS9gib7KtGt1timKs5ZE',
+                'access_token_key': '729613355871285248-PtEMZZ5dK6mmLzEysRUfqA3SRbNhMkQ',
+                'access_token_secret': 'W4v5LHD32xmHY0WzVrjAbFYYSZK0Rf1YcNHMng7AihC53'}
+    elif option == "5":
+        # Ramon qk token 1
+        return {'consumer_key': '72mA7Xh4lvsQPWdyEmcZsiayM',
+                'consumer_secret': 'LfM1QX1qYmWv5xDji255n9l2BL0CAbUEn1spM7vQpzoWeC1Tsz',
+                'access_token_key': '1732212547-HghLnhNoWU4g80Y8OEoD3srWOPNCBGxCylWQDJX',
+                'access_token_secret': '8PcZYpSLRwtpvwae1BVwa3CbogZjoGD5tDlT2L90huwoy'}
+    elif option == "6":
+        # Ramon qk token 2
+        return {'consumer_key': 'DDz2353AejQAHLmw2CXNQ412P',
+                'consumer_secret': 'r8W9yYz0wkurdLQiT7Bhy7pPNEGNziadz8wvh4188jk51ZPsul',
+                'access_token_key': '1732212547-e8ftmSJeDh4I0xOXdWVuECIyJCI5zrkcB3wB4Je',
+                'access_token_secret': 'KHGQUMTThZEIDCQPt0X23KeWiHJJPBgJO6QNA4HOZ2P6u'}
+    elif option == "7":
+        # Ramon qk token 3
+        return {'consumer_key': 'HlTBgVoqWmbp8px129FhiHvut',
+                'consumer_secret': 'Ow2NJSLVlTEVYk5aeYNASGQ4rIS88SgsqDqP5K8AtBnk6bDcsK',
+                'access_token_key': '1732212547-W9gviCH8DmjoO2nEiVuDv6O4YEdjnvLEYJQQjPH',
+                'access_token_secret': '4NMEEOJ6LBBsvkrOm0eXDaENLGGiZqYgpAQYBxl5r5O4q'}
+    elif option == "8":
+        # Ramon qk token 4
+        return {'consumer_key': 'MG0G8utj2YROPIRYw5f1neE2r',
+                'consumer_secret': 'XYJYYRPxBmGNM3mb85mVRCQmkBp8TYddMxdxWNDcnOTXlBTJWl',
+                'access_token_key': '1732212547-9joNOaJJ3m2HENHBh7x2eF7Kz68LmCoYXMNeuCv',
+                'access_token_secret': 'Is5sN0GwqJD3lSKm3sdLsN8mZ25ZlxWGfHzBghNiD4tF2'}
+    elif option == "9":
+        # Ramon qk token 5
+        return {'consumer_key': 'eonAYEHJUJDQ3MLOH6S4CmnzO',
+                'consumer_secret': '5S7njqmb3L8dRX6q3XESJZp1oNBMXEO2qXV1981NrR7suDgK9t',
+                'access_token_key': '1732212547-PpAuFOa69RBKjbyVkUNiVVRtR9AJzgynGDzxBzT',
+                'access_token_secret': 'JKFSwkJK5xEJLyy8llSVdCE1hsrMx0N5ybKovWRtUhvdm'}
+    elif option == "10":
+        # Bot practicas n1
+        return {'consumer_key': 'SQeJqOREbZQXZpNGQeQcSdfXo',
+                'consumer_secret': 'm4zhiIQ6yiT4Gag0uFpuxVSawsZkUYLdL7sufZXnFeYSiqRQXB',
+                'access_token_key': '795707625979703297-NHI92SIuIHjyf9v7UbgFeyiWB3TcHmA',
+                'access_token_secret': 'WPz1SeGV7Sin3s4LVfw3SVvNVXsH1j5zuxOUURGRuZYPI'}
+    elif option == "11":
+        # Bot practicas n2
+        return {'consumer_key': 'IQ7zcg3xQPOC2qtRzj4rpOHsP',
+                'consumer_secret': '9GCYqgwwuEfxlDV7QNDm7Sel3275ujDaGF8WdRU9fk67LAw3kp',
+                'access_token_key': '795707625979703297-xcXfIaX6faxfwznrdjvevhKol8xyeQ8',
+                'access_token_secret': '26EjnaE45SxoktBn7WNIPAxiF4leuh5JNlRz8AdhdjrCE'}
+    elif option == "12":
+        # Bot practicas n3
+        return {'consumer_key': 'MX03jqS2tZ7fcb6fuOARnNDJS',
+                'consumer_secret': 'YA5p1W7S3RZ0NKXK48F6EaTQKyGX0QS4Yr6qvFF52GlT1q9UfZ',
+                'access_token_key': '795707625979703297-tAuEX114PqFeXdbO3Vb2tYFeQEWUrDW',
+                'access_token_secret': '8e11OAjiV23wd6WJ1OlQRss2133yqHB15WEq9R49TvpjM'}
+    elif option == "13":
+        # Bot practicas n4
+        return {'consumer_key': '8WdAh7VaW3nanHxHy4SYdCeqI',
+                'consumer_secret': 'KMM4xthnSvNRevNhoVYWVfxtZ5wvBphK7NphLWMKVBhlnkWU2R',
+                'access_token_key': '795707625979703297-AJoIuqKTck7UWmmdgXoWUatHtdi54A0',
+                'access_token_secret': '2uzFr4IY0prps35k2cMsdjQRonlEpfiS67pasGytXu1iI'}
+    elif option == "14":
+        # Bot practicas n5
+        return {'consumer_key': 'gGN1fuzJ5AYqQFjJpH7ogA5Iy',
+                'consumer_secret': 'LuWtGe061Uu9uW2JbT4OUhPy6zH0yd6eKUJcwRX1FjcBTeGIWt',
+                'access_token_key': '795707625979703297-6KAX2G5zRU5Rswg5NnFaB8uozzZ9TY2',
+                'access_token_secret': 'YDqlkEawTJQAjwKBmpJ9ESE3uRT0VHAKaJxWyJVHTe7Az'}
+    elif option == "15":
+        # Bot practicas n6
+        return {'consumer_key': 'EUmAjzOjI70rOH6j2M2cLcr5h',
+                'consumer_secret': 'AMGcnKe8XcVvnpxpEevBW0GMOpvw11uSJwktTMbbQzxh7ajcnw',
+                'access_token_key': '795707625979703297-wYCIee6MfGIGqKsk7RBIXzEn5UjHgcC',
+                'access_token_secret': '1536yTq4p4LlSnPLKWfMPIJNomAGvc37EbYwfvXSze3GY'}
+    elif option == "16":
+        # Bot practicas n7
+        return {'consumer_key': 'HfN7npkNlqY5AdSFGKTNpZnnr',
+                'consumer_secret': 'Eazd8jHJKEsb6KwOvwxD7ikwvluAgaIorF1nKkyofguNzJwLRB',
+                'access_token_key': '795707625979703297-KF2rGUKLDygvxZ3611wwuRpTPxBjzqt',
+                'access_token_secret': 'W0THCgQAhiWAwIq14TS1Oy3wpM81LOjj6KaCIIWQteXoQ'}
+    elif option == "17":
+        # Bot practicas n8
+        return {'consumer_key': 'uIdbvQPs8Ega9CeWi9xL3WkCt',
+                'consumer_secret': 'dH5DU7cDziseJwhSmQHjL0zW9mq5YDnpKxVPJYQiIfOfYCADWt',
+                'access_token_key': '795707625979703297-8QU6LLqeib6wRUMn62dodcyqTqI91Cf',
+                'access_token_secret': '86GaQ1bfNDxGz0LwS5vJrR4QdXfPeLMI83gHJ3vfK7ynZ'}
+    elif option == "18":
+        # Bot practicas n9
+        return {'consumer_key': 'MWVY2TzIm2w2uTnU5nRLKzyig',
+                'consumer_secret': 'X7MeOStZCJk6NGTIqtmn59tKPIW5cgd0MvT5CK9SaQNw3esnj2',
+                'access_token_key': '795707625979703297-O9qfwPg1aUwkTu29xK1irPLVpzruT9U',
+                'access_token_secret': 'olCpm5VuWi9ceYTSr1PCsdWfN6jOkgdf70pbgk5iGPbjC'}
+    elif option == "19":
+        # Bot practicas n10
+        return {'consumer_key': 'uPIXiKiTGkP59f5NRdPHKhX27',
+                'consumer_secret': 'padQSb3zHRYqYCQQFhC9IXznEOlvMITHGtrK2FDV4p6PTvvxnQ',
+                'access_token_key': '795707625979703297-8bmIuCIxspXxVT53241oEQdRQie4nxZ',
+                'access_token_secret': 'EfVSoZ6uNVCpHUxGwJzVpbUwVXJHDLAtGvErmB4CcJSC1'}
+    elif option == "20":
+        # Bot practicas n11
+        return {'consumer_key': 'OVZRDvYvcNAkeSzPxpKt7ypWp',
+                'consumer_secret': '51PI2USL4C1T8rGB8JiOkjUV1x3wRRk32JtIjyBtlQEuTVR3Vv',
+                'access_token_key': '795707625979703297-hW8x902kO0kK76nhxIb3dFYtTI6mIZ8',
+                'access_token_secret': 'XNMqWJx8U8Imao0p9yEhPh30lM0QQ2449E15F8GVBlmtn'}
+    elif option == "21":
+        # Bot practicas n12
+        return {'consumer_key': 'KlJkKKhJohQnIpTKO7bIM0v6c',
+                'consumer_secret': 'aHagqhffj9gXT0fpT7x4DoDxQuvBzWRgr687QxMqz5ObpAEY0Z',
+                'access_token_key': '795707625979703297-Pp7PTGnyIutnzwpHXrE5C1lTJIjD683',
+                'access_token_secret': 'uZ0DfDUcl6fd16QmLiy1MVpNKQ5VPexrxeCdLTOvC5hwM'}
+    elif option == "22":
+        # Bot practicas n13
+        return {'consumer_key': 'UbLuqS7JRtV4jocoQsfh5mSzA',
+                'consumer_secret': 'mTQKkH4qCZmQt0ATEKh1cYVTz3aFYddyb6DSiwflZzCRraBIqJ',
+                'access_token_key': '795707625979703297-oydtpCRQ1ICV32LXoswBrXw91ce0ELr',
+                'access_token_secret': 'uaJIPjWWNk9xHUETl3DVuwlzCyXTSkdb9JSVmFl68t4MB'}
+    elif option == "23":
+        # Bot practicas n14
+        return {'consumer_key': 'gC6XiTrYtEWlbq9IJk7yzCgzN',
+                'consumer_secret': 'k9Fdvsl52UxSmH5RQ0ZtGeQu5eGgGHmTu981zuyAKbpYVVKIcP',
+                'access_token_key': '795707625979703297-p5MSa7UAUKaa1MYdfhGWpUwyADRELpH',
+                'access_token_secret': 'huwgpuXYRdKbrXTIeeammpgpZs6K8sNXgOCJVuCkENBfM'}
+    elif option == "24":
+        # Bot practicas n15
+        return {'consumer_key': 'hgBeIaJuCdgLgBU4V9zCL6MTP',
+                'consumer_secret': 'hPJGiZcsEvppRXq9NWJiZhBw6lJ2jyCpJT427uQOumkgngYwpM',
+                'access_token_key': '795707625979703297-4JDngZC7498hnjcixdPqF4PUyfCZ61d',
+                'access_token_secret': 'kWoKO0stLqTUO6dBlSY7Utx3kADPI4RogYyMe5atReCNZ'}
+
+    elif option == "25":
+        # Bot practicas n16
+        return {'consumer_key': 'LSuaNYoabDW8Pnq4XP9RKHAfK',
+                'consumer_secret': 'n0IYviNymZYnBWn6h7CmLCZSv7iKzXJrtxWfadJ1YZbKZQ0a0X',
+                'access_token_key': '795707625979703297-dQ5QgP6nogMoRQUvqP504MrHd7NNaTf',
+                'access_token_secret': 'CPmcQKE35cTGX30mKjY9HTSS9g4ic5WBuvCgFP5b9QWCt'}
+
+    elif option == "26":
+        # Bot practicas n17
+        return {'consumer_key': 'ZvKjip4jkfqt1VDUupjm2bU6b',
+                'consumer_secret': '2RtCCC1iAp1WLhUipkg0C3tF3gh25Clfrnr0uUcBZHgkI0tXQp',
+                'access_token_key': '795707625979703297-L6SVgF7Hs9qJ9B6NbuPkS5PywT5p7Ay',
+                'access_token_secret': 'Mhuy2zJDvKlDPg5sKbPhVTG4H6CUnsOrYIw1tXf8sbkbH'}
+
+    elif option == "27":
+        # Bot practicas n18
+        return {'consumer_key': 'W4nycyYksOgE38EqmuXQdcQop',
+                'consumer_secret': '4aBedsfP9Q1cxF4Aoy6xHvbfF80v6inMPThWJOibteXaCml040',
+                'access_token_key': '795707625979703297-qjIqhPq6oxY3tL88pcFx0yqEf9ahoMw',
+                'access_token_secret': 'DnmxyuD6aWmrQyo4opQdURbaJgjUSTCvtbvI1Wlgtofpz'}
+
+    elif option == "28":
+        # Bot practicas n19
+        return {'consumer_key': 'E8r5Kj3Mz2ZMmZRrNCtS3YyZZ',
+                'consumer_secret': 'ZK3gjSXijx3V24LyaXnYDr5veWpyb2ibPzvwzosGTMN5ZvWSmy',
+                'access_token_key': '795707625979703297-j5Ua8dTUFGTCudPewtQbzXKYz0ZpRKL',
+                'access_token_secret': 'DYxPGPgpA0wpKxt3EliuMi7pqO2b5M0C42cZq2CSp7qhY'}
+    elif option == "29":
+        # Bot practicas n20
+        return {'consumer_key': 'VE4LNMOhMzsvrilum4PrqhJqp',
+                'consumer_secret': 'GvJJ3bKnCGzQONAyCsXL4MxdEOcniZ3pOEmYI7HLZBttTSEsr3',
+                'access_token_key': '795707625979703297-t4tB87TCKRm4wxWB4f8c03feN6ZPgQr',
+                'access_token_secret': 'azisJOw8KNSlLp4NF67MsfKCEZTFLBUwdjjrCzVOuO9nG'}
+    elif option == "30":
+        # Bot practicas n21
+        return {'consumer_key': 'JK8ZVEJ6Son3SeK4AdGDzh7BX',
+                'consumer_secret': 'SkhMozMxXmtQOrm2nz9R04xXObnAYfxQ4BFxocTVloc87RsWOB',
+                'access_token_key': '795707625979703297-K1uZIOqYpd4XKS5dsETYnBHwx0chiZ1',
+                'access_token_secret': 'evkGSsEZxBoC0BfFU4r9aC2i28x2TRyUgM3InEcFg4Fbe'}
+    elif option == "31":
+        # Bot practicas n22
+        return {'consumer_key': 'vsbwqo8XrKPhBfzB0FEasGvEi',
+                'consumer_secret': 'CKpG2xnpqSi1mCFgfpjV4r4NdQxYDEImyT5hzHW4p3WVfCmzvE',
+                'access_token_key': '795707625979703297-56euLwFTUEDfxOCIZpvx5NeWrWJOq3O',
+                'access_token_secret': 'KaQsnUx3NIb2oKCnzYzheK2P8S6AfTAGkfZfF5KLeonJ7'}
+    elif option == "32":
+        # Bot practicas n23
+        return {'consumer_key': 'vV4987GQ91sfHBdOadWb2Akv7',
+                'consumer_secret': 'g6KO11IoG7XaqDqV0JlkDJtdsvukiwQmCPbhBx5pnn6skrT9AQ',
+                'access_token_key': '795707625979703297-Dikr78FL063dLq9Qnxpg2ALsUcHE4Fs',
+                'access_token_secret': 'mrfirPmX3oJaeTbCV5F8elIUY8yw88YTRZmrRwcyg9G4k'}
+    elif option == "33":
+        # Bot practicas n24
+        return {'consumer_key': 'mxCc5YOKrjPh8LFoEX4nBVdJL',
+                'consumer_secret': '0plnQHtsSpGAwUFaXqcsiwUrJaIQvFP2CmD3VaSZgyCybAozzh',
+                'access_token_key': '795707625979703297-i1NW9KqwULfBI9nHaOM8Q1Ce3ijDo5H',
+                'access_token_secret': 'uOiq7qdjTeaQKyIZIL7g052nJE5Bbfq1wd0CL9GAE0ae2'}

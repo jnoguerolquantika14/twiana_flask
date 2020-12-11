3deba6e8a17988f024db25699709f5b7de5965eb
jnoguerolquantika14 <jnoguerol@quantika14.com>
2020-12-11 19:01:08 +0100

    Flask y Logit_Binom
    API flask y nuevas funciones de prediccion

--- Logit_Binom.py
+++ Logit_Binom.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import json
+import os
 import numpy as np
 import joblib
 from sklearn.linear_model import LogisticRegression
@@ -8,60 +9,29 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
 import pickle
 
-def modelo_Logit_binom(json_viejos):
-    with open(json_viejos) as f:
+global CURR_DIR
+CURR_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def parse_json_attrs(user,politic_parties,attribute):
+    tamo = user.pop(attribute) #Se toma todo el contenido del atributo
+    for party in politic_parties:
+        user[attribute+"_"+party] = tamo[party] #Se extrae el valor de cada array del atributo
+
+def modelo_Logit_binom(json_entrenamiento):
+    with open(json_entrenamiento) as f:
         data = json.load(f)
     # Creación de las variables anidadas en las listas
+    partidos = ["pp", "psoe", "vox", "podemos", "cs", "pacma"]
     for user in data:
-        # Account
-        tamo = user.pop("account")
-        user["account_pp"] = tamo["pp"]
-        user["account_psoe"] = tamo["psoe"]
-        user["account_vox"] = tamo["vox"]
-        user["account_podemos"] = tamo["podemos"]
-        user["account_cs"] = tamo["cs"]
-        user["account_pacma"] = tamo["pacma"]
-        # Username
-        tamo2 = user.pop("username")
-        user["username_pp"] = tamo2["pp"]
-        user["username_psoe"] = tamo2["psoe"]
-        user["username_vox"] = tamo2["vox"]
-        user["username_podemos"] = tamo2["podemos"]
-        user["username_cs"] = tamo2["cs"]
-        user["username_pacma"] = tamo2["pacma"]
-        # Description
-        tamo3 = user.pop("description")
-        user["description_pp"] = tamo3["pp"]
-        user["description_psoe"] = tamo3["psoe"]
-        user["description_vox"] = tamo3["vox"]
-        user["description_podemos"] = tamo3["podemos"]
-        user["description_cs"] = tamo3["cs"]
-        user["description_pacma"] = tamo3["pacma"]
-        # Tweets - Positivos
-        tamo4 = user.pop("tweets-pos")
-        user["tweets-pos_pp"] = tamo4["pp"]
-        user["tweets-pos_psoe"] = tamo4["psoe"]
-        user["tweets-pos_vox"] = tamo4["vox"]
-        user["tweets-pos_podemos"] = tamo4["podemos"]
-        user["tweets-pos_cs"] = tamo4["cs"]
-        user["tweets-pos_pacma"] = tamo4["pacma"]
-        # Tweets - Negativos
-        tamo5 = user.pop("tweets-neg")
-        user["tweets-neg_pp"] = tamo5["pp"]
-        user["tweets-neg_psoe"] = tamo5["psoe"]
-        user["tweets-neg_vox"] = tamo5["vox"]
-        user["tweets-neg_podemos"] = tamo5["podemos"]
-        user["tweets-neg_cs"] = tamo5["cs"]
-        user["tweets-neg_pacma"] = tamo5["pacma"]
-        # Tweets - Neutros
-        tamo6 = user.pop("tweets-neu")
-        user["tweets-neu_pp"] = tamo6["pp"]
-        user["tweets-neu_psoe"] = tamo6["psoe"]
-        user["tweets-neu_vox"] = tamo6["vox"]
-        user["tweets-neu_podemos"] = tamo6["podemos"]
-        user["tweets-neu_cs"] = tamo6["cs"]
-        user["tweets-neu_pacma"] = tamo6["pacma"]
+        parse_json_attrs(user,partidos,"account") # Account
+        parse_json_attrs(user,partidos,"username") # Username
+        parse_json_attrs(user,partidos,"description") # Description
+        parse_json_attrs(user,partidos,"tweets-pos") # Tweets - Positivos
+        parse_json_attrs(user,partidos,"tweets-neg") # Tweets - Negativos
+        parse_json_attrs(user,partidos,"tweets-neu") # Tweets - Neutros
+        
     datos_final = pd.DataFrame(data)
+    
     # Arreglo de valores de la variable iv
     datos_final.loc[datos_final['iv'] == 'pp\n', 'iv'] = 'pp'
     datos_final.loc[datos_final['iv'] == 'psoe\n', 'iv'] = 'psoe'
@@ -69,10 +39,14 @@ def modelo_Logit_binom(json_viejos):
     datos_final.loc[datos_final['iv'] == 'podemos\n', 'iv'] = 'podemos'
     datos_final.loc[datos_final['iv'] == 'cs\n', 'iv'] = 'cs'
     datos_final.loc[datos_final['iv'] == 'pacma\n', 'iv'] = 'pacma'
+    
     # Eliminación de votantes de upyd
     datos_final = datos_final.drop(datos_final[datos_final['iv'] == "upyd\n"].index)
+    
     datos_final["iv"] = datos_final["iv"].astype('category')
+    
     datos_final_binom = pd.get_dummies(datos_final, columns = ["iv"])
+    
     datos_final_binom = datos_final_binom.reindex(
         columns = ["name", "iv_pp", "iv_psoe", "iv_vox", "iv_podemos", "iv_cs", "iv_pacma", 'account_pp',
                    'account_psoe',
@@ -88,95 +62,60 @@ def modelo_Logit_binom(json_viejos):
                    'tweets-neu_pp', 'tweets-neu_psoe', 'tweets-neu_vox',
                    'tweets-neu_podemos', 'tweets-neu_cs', 'tweets-neu_pacma'])
     datos_final_binom['iv'] = datos_final['iv']
-    partidos = ["pp", "psoe", "vox", "podemos", "cs", "pacma"]
+
     modelos = {}
-    data_X_train = datos_final_binom.iloc[:, 7:43]
+    
+    data_X_train = datos_final_binom.iloc[:, 7:43] #Para dejar fuera al nombre y los account_x
+    
     escalar = StandardScaler()
     X_train = escalar.fit_transform(data_X_train)
+    print(data_X_train)
     with open("obj.pickle", "wb") as f:
         pickle.dump(escalar, f)
-    for i in range(6):
+    for i in range(len(partidos)):
         Y_train =  datos_final_binom.iloc[:, i + 1]
+        
         LR = LogisticRegression().fit(X_train, Y_train)
+        
         joblib.dump(LR, f'modelo_{partidos[i]}.pkl')
 
 
-def funcion_ev_Logit_binom(json_nuevos):
-    with open(json_nuevos) as f:
+def funcion_ev_Logit_binom(json_user):
+    with open(json_user) as f:
         data2 = json.load(f)
     # Creación de las variables anidadas en las listas
+    partidos = ["pp", "psoe", "vox", "podemos", "cs", "pacma"]
     for user in data2:
-        # Account
-        tamo = user.pop("account")
-        user["account_pp"] = tamo["pp"]
-        user["account_psoe"] = tamo["psoe"]
-        user["account_vox"] = tamo["vox"]
-        user["account_podemos"] = tamo["podemos"]
-        user["account_cs"] = tamo["cs"]
-        user["account_pacma"] = tamo["pacma"]
-        # Username
-        tamo2 = user.pop("username")
-        user["username_pp"] = tamo2["pp"]
-        user["username_psoe"] = tamo2["psoe"]
-        user["username_vox"] = tamo2["vox"]
-        user["username_podemos"] = tamo2["podemos"]
-        user["username_cs"] = tamo2["cs"]
-        user["username_pacma"] = tamo2["pacma"]
-        # Description
-        tamo3 = user.pop("description")
-        user["description_pp"] = tamo3["pp"]
-        user["description_psoe"] = tamo3["psoe"]
-        user["description_vox"] = tamo3["vox"]
-        user["description_podemos"] = tamo3["podemos"]
-        user["description_cs"] = tamo3["cs"]
-        user["description_pacma"] = tamo3["pacma"]
-        # Tweets - Positivos
-        tamo4 = user.pop("tweets-pos")
-        user["tweets-pos_pp"] = tamo4["pp"]
-        user["tweets-pos_psoe"] = tamo4["psoe"]
-        user["tweets-pos_vox"] = tamo4["vox"]
-        user["tweets-pos_podemos"] = tamo4["podemos"]
-        user["tweets-pos_cs"] = tamo4["cs"]
-        user["tweets-pos_pacma"] = tamo4["pacma"]
-        # Tweets - Negativos
-        tamo5 = user.pop("tweets-neg")
-        user["tweets-neg_pp"] = tamo5["pp"]
-        user["tweets-neg_psoe"] = tamo5["psoe"]
-        user["tweets-neg_vox"] = tamo5["vox"]
-        user["tweets-neg_podemos"] = tamo5["podemos"]
-        user["tweets-neg_cs"] = tamo5["cs"]
-        user["tweets-neg_pacma"] = tamo5["pacma"]
-        # Tweets - Neutros
-        tamo6 = user.pop("tweets-neu")
-        user["tweets-neu_pp"] = tamo6["pp"]
-        user["tweets-neu_psoe"] = tamo6["psoe"]
-        user["tweets-neu_vox"] = tamo6["vox"]
-        user["tweets-neu_podemos"] = tamo6["podemos"]
-        user["tweets-neu_cs"] = tamo6["cs"]
-        user["tweets-neu_pacma"] = tamo6["pacma"]
+        parse_json_attrs(user,partidos,"account") # Account
+        parse_json_attrs(user,partidos,"username") # Username
+        parse_json_attrs(user,partidos,"description") # Description
+        parse_json_attrs(user,partidos,"tweets-pos") # Tweets - Positivos
+        parse_json_attrs(user,partidos,"tweets-neg") # Tweets - Negativos
+        parse_json_attrs(user,partidos,"tweets-neu") # Tweets - Neutros
     datos_final_nuevos = pd.DataFrame(data2)
     ##########################################################
-    partidos = ["pp", "psoe", "vox", "podemos", "cs", "pacma"]
+    
     clasif = pd.DataFrame([])
     probab = pd.DataFrame([])
     clasif['usuario'] = datos_final_nuevos.iloc[:, 0]
     probab['usuario'] = datos_final_nuevos.iloc[:, 0]
-    data_X_test = datos_final_nuevos.iloc[:, 1:]
+    
+    data_X_test = datos_final_nuevos.iloc[:, 1:] # Quita la columna name
+
     with open("obj.pickle", "rb") as f:
         escalar = pickle.load(f)
     X_test = escalar.transform(data_X_test)  
-    for i in range(6):
+
+    for i in range(len(partidos)):
         # Modelos Logisticos
         LR = joblib.load(f'modelo_{partidos[i]}.pkl')
-        Y_pred = LR.predict(X_test)
-        Y_prob = LR.predict_proba(X_test)
-        clasif[f'{partidos[i]}'] = Y_pred
-        probab[f'{partidos[i]}'] = Y_prob[:, 1]
+        Y_pred = LR.predict(X_test) # Predice la etiqueta
+        Y_prob = LR.predict_proba(X_test) # Estimación de probabilidad
+        
+        clasif[f'{partidos[i]}'] = Y_pred  # Etiquetas (partidos) más probables
+        probab[f'{partidos[i]}'] = Y_prob[:, 1] # Probabilidades de cada partido
+
     clasif["clasif2"] = probab[['pp', 'psoe', 'vox', 'podemos', 'cs', 'pacma']].idxmax(axis = 1)
-    print(clasif.iloc[:,[0,7]])
 
-
-'''
-modelos(datos_viejos_json)
-funcion_completa2(datos_nuevos_json)
-'''
+    pred=clasif.iloc[:,[0,7]]
+    print(pred)
--- app.py
+++ app.py
@@ -1,71 +1,47 @@
-import os
-from IPython.core import display as ICD
-import pandas as pd
 from flask import Flask, jsonify, request, Response
 from flask_pymongo import PyMongo
 from bson import json_util
 from bson.objectid import ObjectId
 import json
-
-from werkzeug.security import generate_password_hash, check_password_hash
+import twiana_nuevo
+import tweepy
+from pymongo import MongoClient
+import time
 
 app = Flask(__name__)
 
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/twianamongodb'
+app.config['MONGO_DBNAME'] = 'twiana'
+#app.config['MONGO_URI'] = 'mongodb://localhost:27017/twiana'
+app.config['MONGO_URI'] = 'mongodb+srv://flask:flask@cluster0.r2dhr.mongodb.net/twiana'
 
 mongo = PyMongo(app)
-
-global CURR_DIR
-CURR_DIR = os.path.dirname(os.path.abspath(__file__))
-
-def tabla_diccionario(diccionario,key_diccionario_input,value_diccionario_input,key_diccionario_output,value_diccionario_output): #Para crear una tabla a partir de un diccionario
-    #Se especifica el diccionario, su key y value, y la key y value que tendrá en la tabla resultante
-    longitud_diccionario=len(diccionario)
-    #Creamos las filas para los nuevos key y value
-    nuevo_key=[0]*(longitud_diccionario)
-    nuevo_value=[0]*(longitud_diccionario)
-    for i in range(longitud_diccionario):
-        #Defimos cada valor de las 2 filas del diccionario
-        nuevo_key[i]=diccionario[i][key_diccionario_input]
-        nuevo_value[i]=diccionario[i][value_diccionario_input]
-
-    # Metodo para crear una tabla: https://stackoverflow.com/questions/60076770/power-bi-dataframe-table-visualization
-    dataset = pd.DataFrame({key_diccionario_output: nuevo_key, value_diccionario_output: nuevo_value},index=['']*longitud_diccionario)
-    return dataset
-
-
-@app.route('/data_ejemplo', methods=['GET'])
-def get_data():
-    f = open(CURR_DIR+'/data_ejemplo.json')
-    data_ejemplo = json.load(f)
-
-    response = json_util.dumps(data_ejemplo, indent=2)
-    # print(response)
     
-    return Response(response, mimetype="application/json")
-
-
-'''
-@app.route('/service/prediction/account', methods=['POST'])
-def account_prediction:
-    return 
-'''
-
-
-@app.route('/service/analytics/city/<city>', methods=['GET'])
-def city_analytics(city):
-    f = open(CURR_DIR+'/data_ejemplo.json')
-    data_ejemplo = json.load(f)
-    json_data = data_ejemplo['data']
-    json_worries = data_ejemplo['worries']
-    json_undecided = data_ejemplo['undecided']
-    # json_dict={'json_data':json_data,'json_worries':json_worries,'json_undecided':json_undecided}
-    #json_dict = (json_data, json_worries, json_undecided)
-    tabla=tabla_diccionario(json_data,)
-    ICD.display()
+@app.route('/service/prediction/account/<account>', methods=['GET'])
+def account_prediction(account):
     
-    response = json_util.dumps(json_data, indent=2)
-    return Response(response)
+
+    start_time = time.time()
+    mongo.db.twiana_twitter.delete_many( { } )
+
+    tweets_limit = request.args.get('limit', default = 200, type = int) # El maximo de tweets sin usar cursor son 200 (si se usa llegaría hasta 3200)
+    json_result=twiana_nuevo.create_json_result(account,tweets_limit)
+
+    id = mongo.db.twiana_twitter.insert_one(
+            {'name+limit':json_result['name']+', '+str(tweets_limit),'result' : json_result})
+    response = jsonify({
+            '_id': str(id),
+            'name':json_result['name'],
+            'result' : json_result
+        })
+    response.status_code = 201
+    '''
+    for x in mongo.db.twiana_twitter.find({'name':account}):
+        print(x)'''
+
+    response_json = json_util.dumps(json_result, indent=2)
+
+    print("--- Finished in %s seconds ---" % (time.time() - start_time))
+    return Response(response_json, mimetype="application/json")
 
 
 if __name__ == "__main__":

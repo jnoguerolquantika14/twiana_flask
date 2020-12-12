d786e90429339a6664fc2d233ab2d8aa0359dac9
jnoguerolquantika14 <jnoguerol@quantika14.com>
2020-12-12 02:16:14 +0100

    Unión de funciones
    Se han unido las funciones en un solo archivo

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
+from os import replace
 from flask import Flask, jsonify, request, Response
 from flask_pymongo import PyMongo
 from bson import json_util
 from bson.objectid import ObjectId
 import json
-import twiana_nuevo
+import twiana
 import tweepy
 from pymongo import MongoClient
 import time
+
 
 app = Flask(__name__)
 
@@ -19,29 +21,39 @@ mongo = PyMongo(app)
 @app.route('/service/prediction/account/<account>', methods=['GET'])
 def account_prediction(account):
     
-
     start_time = time.time()
     mongo.db.twiana_twitter.delete_many( { } )
 
     tweets_limit = request.args.get('limit', default = 200, type = int) # El maximo de tweets sin usar cursor son 200 (si se usa llegaría hasta 3200)
-    json_result=twiana_nuevo.create_json_result(account,tweets_limit)
+    try:
+        json_user, df_result=twiana.twiana(account,tweets_limit)
 
-    id = mongo.db.twiana_twitter.insert_one(
-            {'name+limit':json_result['name']+', '+str(tweets_limit),'result' : json_result})
-    response = jsonify({
-            '_id': str(id),
-            'name':json_result['name'],
-            'result' : json_result
-        })
-    response.status_code = 201
-    '''
-    for x in mongo.db.twiana_twitter.find({'name':account}):
-        print(x)'''
+    
+        json_result=df_result.to_dict(orient='records')
 
-    response_json = json_util.dumps(json_result, indent=2)
+        id = mongo.db.twiana_twitter.insert_one(
+                {'usuario+limit':json_result[0]['usuario']+', '+str(tweets_limit),'clasificacion' : json_result[0]['clasif2']})
+        
+        response = jsonify({
+                '_id': str(id),
+                'usuario':json_result[0]['usuario'],
+                'resultados':json_user,
+                'clasificacion' : json_result[0]['clasif2']
+            })
+        
+        json_user_result={'resultados':json_user,'clasificacion':json_result[0]['clasif2']}
+        response_json = json_util.dumps(json_user_result, indent=2)
 
-    print("--- Finished in %s seconds ---" % (time.time() - start_time))
-    return Response(response_json, mimetype="application/json")
+        print("--- Finished in %s seconds ---" % (time.time() - start_time))
+        return Response(response_json, mimetype="application/json",status=201)
+    except tweepy.TweepError as e:
+        print(e) 
+
+        return Response(
+        "Usuario no válido",
+        status=404,
+    )
+
 
 
 if __name__ == "__main__":
--- twiana.py
+++ twiana.py
@@ -10,7 +10,15 @@ import random
 import json
 import threading,queue
 import traceback
-import time
+import os
+#Predicciones
+import pandas as pd
+import numpy as np
+import joblib
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.preprocessing import StandardScaler
+import pickle
 
 API = None
 initial_result = {
@@ -53,6 +61,7 @@ def retrieve_data(account,limite_tweets_apionly):
     connect_tweepy(api_key)
 
     # 4.- Obtiene User, Timeline y Descripción de la cuenta
+    
     user = API.get_user(account)
     name = user.name
     description = user.description
@@ -61,7 +70,7 @@ def retrieve_data(account,limite_tweets_apionly):
     # Modo extendido para que no se trunquen los tweets
 
     user_timeline = API.user_timeline(account, count=limite_tweets_apionly, tweet_mode='extended')
-    
+
     try:
         
         # 5.- Computa resultados para la Account, Username y Description
@@ -121,7 +130,6 @@ def process_tweet_types(my_results,user_timeline,tweet_type,q):
     
     q.put({tweet_type:my_results[tweet_type]})
 
-
 def select_api_key():
     option = random.randint(1, 8)
     return keys.twiana_key(str(option))
@@ -143,3 +151,120 @@ def create_json_result(account,tweets_limit):
 
     final_file.write(']')
     return json_result
+
+
+def parse_json_attrs(user,politic_parties,attribute):
+    tamo = user.pop(attribute) #Se toma todo el contenido del atributo
+    for party in politic_parties:
+        user[attribute+"_"+party] = tamo[party] #Se extrae el valor de cada array del atributo
+
+def modelo_Logit_binom(json_entrenamiento):
+    with open(json_entrenamiento) as f:
+        data = json.load(f)
+    # Creación de las variables anidadas en las listas
+    partidos = ["pp", "psoe", "vox", "podemos", "cs", "pacma"]
+    for user in data:
+        parse_json_attrs(user,partidos,"account") # Account
+        parse_json_attrs(user,partidos,"username") # Username
+        parse_json_attrs(user,partidos,"description") # Description
+        parse_json_attrs(user,partidos,"tweets-pos") # Tweets - Positivos
+        parse_json_attrs(user,partidos,"tweets-neg") # Tweets - Negativos
+        parse_json_attrs(user,partidos,"tweets-neu") # Tweets - Neutros
+        
+    datos_final = pd.DataFrame(data)
+    
+    # Arreglo de valores de la variable iv
+    datos_final.loc[datos_final['iv'] == 'pp\n', 'iv'] = 'pp'
+    datos_final.loc[datos_final['iv'] == 'psoe\n', 'iv'] = 'psoe'
+    datos_final.loc[datos_final['iv'] == 'vox\n', 'iv'] = 'vox'
+    datos_final.loc[datos_final['iv'] == 'podemos\n', 'iv'] = 'podemos'
+    datos_final.loc[datos_final['iv'] == 'cs\n', 'iv'] = 'cs'
+    datos_final.loc[datos_final['iv'] == 'pacma\n', 'iv'] = 'pacma'
+    
+    # Eliminación de votantes de upyd
+    datos_final = datos_final.drop(datos_final[datos_final['iv'] == "upyd\n"].index)
+    
+    datos_final["iv"] = datos_final["iv"].astype('category')
+    
+    datos_final_binom = pd.get_dummies(datos_final, columns = ["iv"])
+    
+    datos_final_binom = datos_final_binom.reindex(
+        columns = ["name", "iv_pp", "iv_psoe", "iv_vox", "iv_podemos", "iv_cs", "iv_pacma", 'account_pp',
+                   'account_psoe',
+                   'account_vox', 'account_podemos',
+                   'account_cs', 'account_pacma', 'username_pp', 'username_psoe',
+                   'username_vox', 'username_podemos', 'username_cs', 'username_pacma',
+                   'description_pp', 'description_psoe', 'description_vox',
+                   'description_podemos', 'description_cs', 'description_pacma',
+                   'tweets-pos_pp', 'tweets-pos_psoe', 'tweets-pos_vox',
+                   'tweets-pos_podemos', 'tweets-pos_cs', 'tweets-pos_pacma',
+                   'tweets-neg_pp', 'tweets-neg_psoe', 'tweets-neg_vox',
+                   'tweets-neg_podemos', 'tweets-neg_cs', 'tweets-neg_pacma',
+                   'tweets-neu_pp', 'tweets-neu_psoe', 'tweets-neu_vox',
+                   'tweets-neu_podemos', 'tweets-neu_cs', 'tweets-neu_pacma'])
+    datos_final_binom['iv'] = datos_final['iv']
+
+    modelos = {}
+    
+    data_X_train = datos_final_binom.iloc[:, 7:43] #Para dejar fuera al nombre y los account_x
+    
+    escalar = StandardScaler()
+    X_train = escalar.fit_transform(data_X_train)
+
+    with open("obj.pickle", "wb") as f:
+        pickle.dump(escalar, f)
+    for i in range(len(partidos)):
+        Y_train =  datos_final_binom.iloc[:, i + 1]
+        
+        LR = LogisticRegression().fit(X_train, Y_train)
+        
+        joblib.dump(LR, f'modelo_{partidos[i]}.pkl')
+
+def funcion_ev_Logit_binom(json_user):
+    with open(json_user) as f:
+        data2 = json.load(f)
+    # Creación de las variables anidadas en las listas
+    partidos = ["pp", "psoe", "vox", "podemos", "cs", "pacma"]
+    for user in data2:
+        parse_json_attrs(user,partidos,"account") # Account
+        parse_json_attrs(user,partidos,"username") # Username
+        parse_json_attrs(user,partidos,"description") # Description
+        parse_json_attrs(user,partidos,"tweets-pos") # Tweets - Positivos
+        parse_json_attrs(user,partidos,"tweets-neg") # Tweets - Negativos
+        parse_json_attrs(user,partidos,"tweets-neu") # Tweets - Neutros
+    datos_final_nuevos = pd.DataFrame(data2)
+    ##########################################################
+    
+    clasif = pd.DataFrame([])
+    probab = pd.DataFrame([])
+    clasif['usuario'] = datos_final_nuevos.iloc[:, 0]
+    probab['usuario'] = datos_final_nuevos.iloc[:, 0]
+    
+    data_X_test = datos_final_nuevos.iloc[:, 1:] # Quita la columna name
+
+    with open("obj.pickle", "rb") as f:
+        escalar = pickle.load(f)
+    X_test = escalar.transform(data_X_test)  
+
+    for i in range(len(partidos)):
+        # Modelos Logisticos
+        LR = joblib.load(f'modelo_{partidos[i]}.pkl')
+        Y_pred = LR.predict(X_test) # Predice la etiqueta
+        Y_prob = LR.predict_proba(X_test) # Estimación de probabilidad
+        
+        clasif[f'{partidos[i]}'] = Y_pred  # Etiquetas (partidos) más probables
+        probab[f'{partidos[i]}'] = Y_prob[:, 1] # Probabilidades de cada partido
+
+    clasif["clasif2"] = probab[['pp', 'psoe', 'vox', 'podemos', 'cs', 'pacma']].idxmax(axis = 1)
+
+    pred=clasif.iloc[:,[0,7]]
+    return pred
+
+global CURR_DIR
+CURR_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def twiana(account,limite_tweets_apionly):
+    json_user=create_json_result(account,limite_tweets_apionly)
+    json_entrenamiento=modelo_Logit_binom(CURR_DIR+'/results-v3.json')
+    df_result=funcion_ev_Logit_binom(CURR_DIR+'/results.json')
+    return json_user, df_result
